f2c444700fbe6f5a8e5963b1a32d375bac809323
Abra19 <evabrarova@gmail.com>
2024-02-20 00:38:25 +0300

    add query handler

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/hello', (req, res) => {
+  const query = req.url.slice(req.url.indexOf('?') + 1)
+  const params = new URLSearchParams(query);
+  let name = params.get('name') ? params.get('name') : 'World';
+  res.send(`Hello ${name}`);
+});
+
+
+
+
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
