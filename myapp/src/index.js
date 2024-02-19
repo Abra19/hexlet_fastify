8413de5c5ed180339785230674fa3fa0c2b0323b
Abra19 <evabrarova@gmail.com>
2024-02-20 01:11:01 +0300

    add dynamic routes

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,7 +1,15 @@
 import fastify from 'fastify';
+import middie from '@fastify/middie';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
 const app = fastify();
 const port = 3000;
+const logger = morgan('combined');
+
+await app.register(middie);
+app.use(logger);
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -14,6 +22,21 @@ app.get('/hello', (req, res) => {
   res.send(`Hello ${name}`);
 });
 
+app.get('/courses/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.id}`);
+});
+
+app.get('/users/:id', (req, res) => {
+  res.send(`User ID: ${req.params.id}`);
+});
+
+app.get('/courses/:courseId/lessons/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.id}`);
+});
+
+app.get('/users/:id/post/:postId', (req, res) => {
+  res.send(`Users ID: ${req.params.id}; Post ID: ${req.params.postId}`);
+});
 
 
 
